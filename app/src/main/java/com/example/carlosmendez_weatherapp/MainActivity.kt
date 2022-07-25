0c9ebe75eeaecf9fa541ee0af79ee7574c98e4b4
NMSamDev <c_s_m_h_13@hotmail.com>
2022-07-24 20:16:37 -0400

    response and dependencies

--- app/src/main/java/com/example/carlosmendez_weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/carlosmendez_weatherapp/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.carlosmendez_weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+// API KEY = 4a46bc43c4eb9dcbf74bbd28bf8b73e3
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/example/carlosmendez_weatherapp/model/WeatherResponse.kt
@@ -0,0 +1,60 @@
+package com.example.carlosmendez_weatherapp.model
+
+data class WeatherResponse(
+    val list: List<WeatherListItem>
+)
+
+data class WeatherListItem(
+    val main: MainW,
+    val weather: WeatherItemW,
+    val clouds: CloudsW,
+    val wind: WindW,
+    val sys: SysW,
+    val dt_txt: String,
+    val visibility: Int,
+    val city: CityW
+)
+
+data class MainW(
+    val temp: Float,
+    val feels_like: Float,
+    val temp_min: Float,
+    val temp_max: Float,
+    val pressure: Int,
+    val sea_level: Int,
+    val grnd_level: Int,
+    val humidity: Int,
+    val temp_kf: Float
+)
+
+data class WeatherItemW(
+    val main: String,
+    val description: String,
+    val icon: String
+)
+
+data class CloudsW(
+    val all: Int
+)
+
+data class WindW(
+    val speed: Float,
+    val deg: Int,
+    val gust: Float
+)
+
+data class SysW(
+    val pod: String
+)
+
+data class CityW(
+    val name: String,
+    val country: String,
+    val coord: CoordCity
+)
+
+data class CoordCity(
+    val lat: Float,
+    val lon: Float
+)
+
